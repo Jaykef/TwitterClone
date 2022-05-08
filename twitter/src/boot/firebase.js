8abeecc430113e8119d98b863f4b9b0190e892bf
Jaward Sesay <jkef96@icloud.com>
2022-05-08 18:53:03 +0800

    Update firebase.js

--- twitter/src/boot/firebase.js
+++ twitter/src/boot/firebase.js
@@ -3,13 +3,7 @@ import "firebase/firestore"
 
 const firebaseConfig = {
   // YOUR CONFIG HERE
-  apiKey: "AIzaSyAS99udaRZ82ljTp91_Q0ndE38owPPs-PY",
-  authDomain: "qwitter-8dd69.firebaseapp.com",
-  projectId: "qwitter-8dd69",
-  storageBucket: "qwitter-8dd69.appspot.com",
-  messagingSenderId: "986306252325",
-  appId: "1:986306252325:web:91a4de24de1cbd8edff9cc",
-  measurementId: "G-WCVWETTKWV"
+  
 }
 
 firebase.initializeApp(firebaseConfig)
